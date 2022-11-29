965b65b3cd2dd3a97808337c68bce29fee829ef5
Alexandr <dajj2012@mail.ru>
2022-11-29 11:53:52 +0300

    Задача 1 готова

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,3 +1,15 @@
 ﻿Console.Clear();
-Console.WriteLine("Введите число");
+Console.WriteLine("Введите число один");
 int n =  int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число два");
+int m =  int.Parse(Console.ReadLine());
+if (n > m) 
+{
+Console.Write("Число один больше ");
+Console.WriteLine(n);
+}
+else
+{
+Console.Write("Число два больше ");
+Console.WriteLine(m);   
+}
