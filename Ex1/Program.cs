d330875d868463c0e9e64a96ebf74fcb222af75d
Alexandr <dajj2012@mail.ru>
2022-11-29 11:44:06 +0300

    начало3

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int n =  int.Parse(Console.ReadLine());
