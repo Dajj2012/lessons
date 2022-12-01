71dca8b90d5f33a3a9bdcbc6f7c63a668e6efb02
Alexandr <dajj2012@mail.ru>
2022-12-01 12:42:30 +0300

    Задача 4

---
+++ Ex4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число один");
+int n =  int.Parse(Console.ReadLine());
+int index=0;
+while (index<=n)
+{
+    if (index % 2 == 0) Console.WriteLine(index);
+    index++;
+
+}
