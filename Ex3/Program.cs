0d9fb8f7ee165d62c4be29c6221cd49c447b29ab
Alexandr <dajj2012@mail.ru>
2022-12-01 12:34:33 +0300

    Задача 3

---
+++ Ex3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число один");
+int n =  int.Parse(Console.ReadLine());
+if (n % 2 == 0) 
+{
+Console.Write("Число четное ");
+}
+else
+{
+Console.Write("Число нечетное "); 
+}
