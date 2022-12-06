05cca506d1377d49dca8373d1f4518ada5cdbbc1
Alexandr <dajj2012@mail.ru>
2022-12-06 11:31:24 +0300

    Сделал дз 3

---
+++ Ex8/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите X1 ");
+double x1 =  double.Parse(Console.ReadLine());
+Console.WriteLine("Введите Y1 ");
+double y1 =  double.Parse(Console.ReadLine());
+Console.WriteLine("Введите Z1 ");
+double z1 =  double.Parse(Console.ReadLine());
+Console.WriteLine("Введите X2 ");
+double x2 =  double.Parse(Console.ReadLine());
+Console.WriteLine("Введите Y2 ");
+double y2 =  double.Parse(Console.ReadLine());
+Console.WriteLine("Введите Z2 ");
+double z2 =  double.Parse(Console.ReadLine());
+double res=Math.Pow(x1-x2,2)+Math.Pow(y1-y2,2)+Math.Pow(z1-z2,2);
+Console.WriteLine(Math.Round(Math.Sqrt(res),2));
---
+++ Ex9/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите N ");
+int n =  int.Parse(Console.ReadLine());
+int i=1;
+while (i<n+1)
+{
+    Console.WriteLine(Math.Pow(i,3));
+    i++;
+}
