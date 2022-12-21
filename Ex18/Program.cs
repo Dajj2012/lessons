220bc9f43effbc4231fdae9e8af677b360a6df71
Alexandr <dajj2012@mail.ru>
2022-12-21 11:06:55 +0300

    Дз_6

---
+++ Ex18/Program.cs
@@ -0,0 +1,18 @@
+﻿void InputArray(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+array[i] = int.Parse(Console.ReadLine()); 
+}
+Console.Clear();
+Console.WriteLine("Введите число ");
+int n =  int.Parse(Console.ReadLine());
+int[] array = new int[n];
+int kol=0;
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+for (int i=0; i<array.Length; i++)
+{
+    if (array[i]>0)
+    kol++;
+}
+Console.WriteLine($"колличество эллементов больше нуля: {kol}");
