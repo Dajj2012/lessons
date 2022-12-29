413c48fb259f4166f7345bab066a03d44bce528b
Alexandr <dajj2012@mail.ru>
2022-12-29 11:14:06 +0300

    Дз_final

---
+++ Ex27/Program.cs
@@ -0,0 +1,18 @@
+﻿int Sum(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + Sum(m, n);
+        return res;
+    }
+}
+Console.Clear();
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write(Sum(m - 1, n));
